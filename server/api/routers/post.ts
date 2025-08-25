1f0e0f085e31ac4949560c171bf9d6f35a28f88c
Hunde-D <hunde.ddh@gmail.com>
2025-08-25 19:44:10 +0300

    getting started

---
+++ server/api/routers/post.ts
@@ -0,0 +1,36 @@
+import z from "zod";
+import { createTRPCRouter, publicProcedure } from "../trpc";
+
+interface Post {
+  id: number;
+  name: string;
+}
+
+const posts: Post[] = [
+  {
+    id: 1,
+    name: "First Post",
+  },
+];
+
+export const postRouter = createTRPCRouter({
+  hello: publicProcedure
+    .input(z.object({ text: z.string() }))
+    .query(({ input }) => {
+      return { greeting: `Hello ${input.text} from the post router!` };
+    }),
+  create: publicProcedure
+    .input(z.object({ name: z.string().min(1) }))
+    .mutation(async ({ input }) => {
+      const post: Post = {
+        id: posts.length + 1,
+        name: input.name,
+      };
+      posts.push(post);
+      return post;
+    }),
+
+  getLatest: publicProcedure.query(() => {
+    return posts.at(-1) ?? null;
+  }),
+});
---
+++ server/api/trpc.ts
@@ -0,0 +1,33 @@
+import superjson from "superjson";
+import { initTRPC } from "@trpc/server";
+import { ZodError } from "zod";
+
+// import { db } from "@/server/db";
+
+export const createTRPCContext = async (opts: { headers: Headers }) => {
+  return {
+    // db,
+    ...opts,
+  };
+};
+
+const t = initTRPC.context<typeof createTRPCContext>().create({
+  transformer: superjson,
+  errorFormatter({ shape, error }) {
+    return {
+      ...shape,
+      data: {
+        ...shape.data,
+        zodError:
+          error.cause instanceof ZodError ? error.cause.flatten() : null,
+      },
+    };
+  },
+});
+
+export const createCallerFactory = t.createCallerFactory;
+
+// Route & Procedure
+
+export const createTRPCRouter = t.router;
+export const publicProcedure = t.procedure;
